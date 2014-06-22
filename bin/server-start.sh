97169059b48a1b96dd38270da66ae0aa9f8cd517
Karthik Viswanathan <karthik.ksv@gmail.com>
2014-06-22 15:06:38 -0700

    Run production start script as deploy user.

--- bin/server-start.sh
+++ bin/server-start.sh
@@ -2,9 +2,10 @@
 
 NAME="evangelist"                    # Name of the application
 DIRECTORY="/home/deploy/evangelist"  # Location of application
-USER=deploy                          # User to run as
  
 echo "Starting $NAME as `whoami`"
  
 cd $DIRECTORY
-exec su $USER -c '. ~/.bash_profile && . env.sh && go run server.go'
+. ~/.bash_profile
+source env.sh
+go run server.go
